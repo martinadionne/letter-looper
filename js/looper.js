8e8387ba90b41fd38df0392e0ca40010984e0d0d
Martina Dionne <dion0100@algonquinlive.com>
2017-01-23 14:36:08 -0500

    Update looper.js

--- js/looper.js
+++ js/looper.js
@@ -1,1 +1,17 @@
+var letter, i, k;
 
+while (!letter) { 
+    letter = prompt("Enter a character"); 
+}
+
+// console.log(letter);
+
+for (var i= 0; i <10; i += 1) {
+    
+    for(k= 0; k<=i; k += 1) { document.write(letter);
+        
+    } 
+    
+    document.write("<br>");
+      
+}
